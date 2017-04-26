5fbeb257da97c3874958457aa80ec11b1598c514
Victor Yu <victoryu@Victors-MacBook-Pro.local>
2017-04-26 09:21:52 -0400

    Added intro screen

---
+++ Assets/Scripts/IntroScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// starts the game
+		if (Input.GetKeyDown (KeyCode.R)) 
+		{
+			//			Application.LoadLevel(0);
+			SceneManager.LoadScene("Scene1", LoadSceneMode.Single);
+			Time.timeScale = 1;
+		}
+	}
+}
